ab0b08ad2b951b80f805862a4b4310eb3f960e49
itzbidyut <bbboruah1994@gmail.com>
2022-09-17 00:22:54 +0530

    updated

--- src/student/queries.js
+++ src/student/queries.js
@@ -3,8 +3,11 @@ const getStudentById = "SELECT * FROM student WHERE id = $1";
 const addstudent =
   "INSERT INTO student (name, age, mark1, mark2, mark3) VALUES ($1,$2,$3,$4,$5)";
 const checkNameExists = "SELECT s FROM student s WHERE s.name=$1";
+const checkNameExistsById = "SELECT s FROM student s WHERE s.id=$1";
 const getStudentPassed = "SELECT * FROM student WHERE (mark1+mark2+mark3) >90";
 const getStudentFailed = "SELECT * FROM student WHERE (mark1+mark2+mark3) <90;";
+const deleteStudent = "DELETE FROM student WHERE id= $1";
+const updateStudent = "UPDATE student SET name=$1,age=$2, mark1=$3, mark2=$4, mark3=$5 WHERE id=$6";
 module.exports = {
   getStudents,
   getStudentById,
@@ -12,4 +15,7 @@ module.exports = {
   checkNameExists,
   getStudentFailed,
   addstudent,
+  deleteStudent,
+  checkNameExistsById,
+  updateStudent,
 };
--- src/student/routes.js
+++ src/student/routes.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
-const { getStudents, getStudentById, postStudent } = require("./controller");
+const {
+  getStudents,
+  getStudentById,
+  postStudent,
+  deleteStudent,
+  updateStudent,
+} = require("./controller");
 
 const router = Router();
 
 router.get("/students/", getStudents);
-router.post("/students/", postStudent);
+router.post("/upload", postStudent);
+router.delete("/students/:id", deleteStudent);
+router.put("/students/:id", updateStudent);
 router.get("/students/:id/result", getStudentById);
 module.exports = router;
